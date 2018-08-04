937614d50acf82dd2a495860508884e257ffdc78
Haythem Sellami <sellamihaythem96@gmail.com>
2018-08-04 21:12:57 +0100

    mint token

--- contracts/certificate.sol
+++ contracts/certificate.sol
@@ -10,19 +10,27 @@ contract Certificate is ERC721Token, Ownable {
     string public constant name = "HCertificate";
     string public constant symbol = "HCERT";
 
-    /*
-    struct Certification {
-        address certified;
-        uint64 declaredAt;
-    }
-
-    Certification[] certifications;
-
-    mapping(uint256 => address) public tokenIndexToOwner;
-    */
-
     constructor() ERC721Token(name, symbol) public { 
 
+    }
+
+    /**
+    * @dev Mints a token to an address with a tokenURI.
+    * @param _to address of the future owner of the token
+    * @param _tokenURI token URI for the token
+    */
+    function mint(address _to, string _tokenURI) public {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _setTokenURI(newTokenId, _tokenURI);
+    }
+
+    /**
+    * @dev calculates the next token ID based on totalSupply
+    * @return uint256 for the next token ID
+    */
+    function _getNextTokenId() private view returns (uint256) {
+        return totalSupply().add(1); 
     }
 
 }
