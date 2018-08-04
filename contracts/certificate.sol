e6ea03bcc8bf48d4b01c6b9e2887c1299233bc94
Haythem Sellami <sellamihaythem96@gmail.com>
2018-08-04 18:26:02 +0100

    refactor code

--- contracts/certificate.sol
+++ contracts/certificate.sol
@@ -11,11 +11,7 @@ contract Certificate is ERC721Token {
     string public constant name = "certificate";
     string public constant symbol = "HCERT";
 
-    bytes4 constant InterfaceID_ERC165 = bytes4(keccak256("supportsInterface(bytes4)"));
-
-    bytes4 constant InterfaceID_ERC721 = 
-    bytes4(keccak256("name()")) ^ bytes4(keccak256("symbol()")) ^ bytes4(keccak256("totalSupply()")) ^ bytes4(keccak256("balanceOf(address)")) ^ bytes4(keccak256("ownerOf(uint256)")) ^ bytes4(keccak256("approve(address, uint256)")) ^ bytes4(keccak256("transfer(address, uint256)")) ^ bytes4(keccak256("transferFrom(address, address, uint256)")) ^ bytes4(keccak256("tokensOfOwner(address)"));
-
+    /*
     struct Certification {
         address certified;
         uint64 declaredAt;
@@ -24,4 +20,5 @@ contract Certificate is ERC721Token {
     Certification[] certifications;
 
     mapping(uint256 => address) public tokenIndexToOwner;
+    */
 }
